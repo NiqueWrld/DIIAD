10c0e9fea5892d589ca19671fb47175d7bcc2532
NiqueWrld <siyathokoza321@gmail.com>
2024-06-09 05:47:23 +0200

    Add Tutorial 11

---
+++ APDA201_WORKBOOK_2024/Tutorial 11/Ibiza Pizza/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ibiza_Pizza.Models;
+
+namespace Ibiza_Pizza.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Calculate()
+        {
+            ViewBag.Message = "Your Calculate page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculate calc)
+        {
+            ViewBag.Message = "Your Calculate page.";
+            calc.amountSpent = calc.calcAmountSpent();
+            calc.change = calc.calcChange();
+            return View(calc);
+        }
+
+    }
+}
---
+++ APDA201_WORKBOOK_2024/Tutorial 11/Ibiza Pizza/Models/Calculate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Ibiza_Pizza.Models
+{
+    public class Calculate
+    {
+        [Required]
+        public double cashTendered { get; set; }
+        [Required]
+        public string pizzaType { get; set; }
+        [Required]
+        [Range(1 , 5)]
+        public int pizzaQuantity { get; set; }
+        public double amountSpent { get; set; }
+        public double change { get; set; }
+
+        public double calcAmountSpent()
+        {
+            double total = 0;
+            if(pizzaType == "Veg")
+            {
+                total = pizzaQuantity * 11.99;
+            }
+            else if(pizzaType == "Chicken")
+            {
+                total = pizzaQuantity * 14.99;
+            }
+            return total;
+        }
+
+        public double calcChange()
+        {
+            double total = 0;
+
+            if(cashTendered > calcAmountSpent())
+            {
+                total = cashTendered - calcAmountSpent();
+            }
+
+            return total;
+        }
+
+    }
+}
